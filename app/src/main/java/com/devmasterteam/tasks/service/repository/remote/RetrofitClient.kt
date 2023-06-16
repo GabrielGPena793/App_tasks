84c90d96075206653f72417e46e4c21a469ba498
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2023-06-16 20:04:18 -0300

    intercepting request

--- app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.devmasterteam.tasks.service.repository.remote
 
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,9 +10,22 @@ class RetrofitClient private constructor() {
     companion object {
         private lateinit var INSTANCE: Retrofit
         private const val BASE_URL = "http://devmasterteam.com/CursoAndroidAPI/"
+        private var token = ""
+        private var personKey = ""
 
         private fun getRetrofitInstance(): Retrofit {
             val http = OkHttpClient.Builder()
+
+            // ====== intercepitando uma request
+            http.addInterceptor { chain ->
+                val request = chain.request()
+                    .newBuilder()
+                    .addHeader(TaskConstants.HEADER.PERSON_KEY, token)
+                    .addHeader(TaskConstants.HEADER.TOKEN_KEY, personKey)
+                    .build()
+
+                chain.proceed(request)
+            }
 
             if (!::INSTANCE.isInitialized) {
                 INSTANCE = synchronized(RetrofitClient::class) {
@@ -28,6 +42,11 @@ class RetrofitClient private constructor() {
 
         fun <T> createService(service: Class<T>): T {
             return getRetrofitInstance().create(service)
+        }
+
+        fun addHeaders(tokenValue: String, personKeyValue : String) {
+            token = tokenValue
+            personKey = personKeyValue
         }
     }
 }
